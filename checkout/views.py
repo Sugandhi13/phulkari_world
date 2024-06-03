69d30f3e082a551a0f02c56cc1d9211baa4df04c
Sugandhi . <sugandhisharma1988@gmail.com>
2024-06-03 11:56:17 +0000

    Added strip elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key':'pk_test_51PNZ29BI916xVvQnAMNGUP6dj9NaijTInt3F7WaEk7UkUNAcw1VavBpJu840pAE5sSYtUebyFXt8VzmLI1qEN0GJ00m5ayZ9Yt',
+        'client_secret':'test client secret',
     }
 
     return render(request, template, context)
